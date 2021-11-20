036b8369924679b8a1951d136b4a7adfc758117f
TaisukeUmetani <TaisukeUmetani@users.noreply.github.com>
2021-11-20 11:46:00 +0000

    deploy: eb1ta1/blog@985486f60c92a173f1bda1969e8cba4015db42fa

--- posts/budoux-on-blog/source/budou_x.py
+++ posts/budoux-on-blog/source/budou_x.py
@@ -1,12 +1,11 @@
 # ./source/budou_x.py
 import sys
 args = sys.argv
-base_path = args[1]
-input_file = args[2]
-output_file = args[3] 
+input_file = args[1]
+output_file = args[2] 
 
 def budou_x_convert():
-    text_file = open(base_path + input_file, 'r')
+    text_file = open(input_file, 'r')
     text = text_file.read()
     text_file.close()
 
@@ -14,13 +13,13 @@ def budou_x_convert():
 
     parser = budoux.load_default_japanese_parser()
     converted_text = parser.translate_html_string(text)
-    print(converted_text)
+    # print(converted_text)
 
-    f = open(base_path + output_file, 'w')
+    f = open(output_file, 'w')
     f.write(converted_text)
     f.close()
 
 budou_x_convert()
 
 # usage
-# $ python3 budou_x.py base_path input_file output_file
+# $ python3 budou_x.py input_file output_file
