3ba0da1b9a371d7c46ad9497906b0857cbaae386
TaisukeUmetani <TaisukeUmetani@users.noreply.github.com>
2021-11-20 08:39:53 +0000

    deploy: eb1ta1/blog@35321513b8f683414568bcd949c1e011fe51a7ca

--- posts/budoux-on-blog/source/budou_x.py
+++ posts/budoux-on-blog/source/budou_x.py
@@ -1,21 +1,15 @@
+# ./source/budou_x.py
 def budou_x_convert(input_file, output_file):
     text_file = open(input_file, 'r')
     text = text_file.read()
     text_file.close()
 
     import budoux
-    import os
 
     parser = budoux.load_default_japanese_parser()
     converted_text = parser.translate_html_string(text)
     print(converted_text)
 
-    if os.path.exists(output_file):
-        f = open(output_file, 'w')
-        f.write(converted_text)
-        f.close()
-        
-    else:
-        f = open(output_file, 'x')
-        f.write(converted_text)
-        f.close()
+    f = open(output_file, 'w')
+    f.write(converted_text)
+    f.close()
